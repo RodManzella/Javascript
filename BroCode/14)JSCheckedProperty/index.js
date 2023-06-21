eccb2fefdd42c7040410bff5e6c57950f6153366
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-06-21 10:36:50 -0300

    commit

--- BroCode/14)JSCheckedProperty/index.js
+++ BroCode/14)JSCheckedProperty/index.js
@@ -1,10 +1,24 @@
-console.log("I like piza");
-console.log("It´s really good");
+// checked property: let us know if a button is selected. Gives us a boolean value.
 
-window.alert("I really love pizza!");
+document.getElementById("myButton").onclick = function () {
+  const myCheckBox = document.getElementById("myCheckBox");
+  const visaBtn = document.getElementById("visaBtn");
+  const mastercardBtn = document.getElementById("masterCardBtn");
+  const paypalBtn = document.getElementById("paypalBtn");
 
-// This is a comment
+  if (myCheckBox.checked) {
+    console.log("You are subscribed!");
+  } else {
+    console.log("You are not subscribed!");
+  }
 
-/*
-This is a multiline comment
- */
+  if (visaBtn.checked) {
+    console.log("You are paying with a visa!");
+  } else if (mastercardBtn.checked) {
+    console.log("You are paying with a mastercard!");
+  } else if (paypalBtn.checked) {
+    console.log("You are paying with PayPal!");
+  } else {
+    console.log("You must select a payment type!");
+  }
+};
