1ac10cd60a1dac11bc58a903d0db7418b9334345
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-06-21 20:21:56 -0300

    commit

--- BroCode/37)spreadOperator/index.js
+++ BroCode/37)spreadOperator/index.js
@@ -1,10 +1,25 @@
-console.log("I like piza");
-console.log("It´s really good");
+// spread operator = allows an iterable, such as an array
+// or string to be expanded in places where zero or more arguments are expected
+// unpacks the elements
 
-window.alert("I really love pizza!");
+let userName = "Bro code";
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-// This is a comment
+//let maximum = Math.max(numbers);
+//console.log(maximum);
 
 /*
-This is a multiline comment
- */
+let maximum = Math.max(...numbers);
+console.log(maximum);
+*/
+
+let class1 = ["Spongebob", "Patrick", "Sandy"];
+let class2 = ["Squidward", "Mr.Krabs", "Plankton"];
+
+//class1.push(class2);  the entire array will be pushed
+
+// class1 = ["Spongebob", "Patrick", "Sandy", array(3)]
+
+class1.push(...class2);
+console.log(class1);
+// ["Spongebob", "Patrick", "Sandy","Squidward", "Mr.Krabs", "Plankton"]
