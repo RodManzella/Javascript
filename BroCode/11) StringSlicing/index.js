9e610eba21a72ba85d0fc07d3e5c2d3dca5a0866
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-06-21 08:03:27 -0300

    commit

--- BroCode/11) StringSlicing/index.js
+++ BroCode/11) StringSlicing/index.js
@@ -1,3 +1,13 @@
 // slice() extracts a section of a string
 // and returns it as a new string,
 // without modifying the original
+
+let fullName = "Bro Code";
+let firstName;
+let lastName;
+
+//firstName = fullName.slice(0, 3); // Bro
+//lastName = fullName.slice(4); //Code
+
+firstName = fullName.slice(0, fullName.indexOf(" "));
+lastName = fullName.slice(fullName.indexOf(" ") + 1);
