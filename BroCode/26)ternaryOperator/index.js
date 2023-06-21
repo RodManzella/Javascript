a03f37a02c7923a72ac41ce7b0209b653e3ad72f
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-06-21 15:47:57 -0300

    COMMIT

---
+++ BroCode/26)ternaryOperator/index.js
@@ -0,0 +1,14 @@
+// ternary operator = Shortcut for an ''if/else statement
+// takes 3 operands
+
+// condition ? exprIfTrue : exprIfFalse
+
+// let adult = checkAge(21);
+
+// function checkAge(age){
+//     return age >=18 ? true : false;
+// }
+
+function checkWinner(win) {
+  win ? console.log("YOU WIN!") : console.log("YOU LOSE!");
+}
