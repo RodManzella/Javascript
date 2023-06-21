1df6eafe8326232e37c10c232603871c5d13c207
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-06-21 17:36:15 -0300

    commit

---
+++ BroCode/33)JSArrays/index.js
@@ -0,0 +1,12 @@
+let fruits = ["apple", "orange", "banana"];
+
+// fruits[0] =  "coconut";
+fruits.push("lemon"); // add an element
+fruits.pop(); // removes last element
+fruits.unshift("mango"); // add element to beginning
+fruits.shift(); // removes element from beginning
+
+console.log(fruits[0]);
+
+let length = fruits.length;
+let index = fruits.indexOf("apple");
