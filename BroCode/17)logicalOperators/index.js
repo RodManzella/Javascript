026b9b526b60941d9fef5008ec54961c4823e06b
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-06-21 12:44:52 -0300

    commit

---
+++ BroCode/17)logicalOperators/index.js
@@ -0,0 +1,10 @@
+let temp = 15;
+let sunny = true;
+
+// && and ||
+
+if (temp > 0 && temp < 30 && sunny) {
+  console.log("The weather is good!");
+} else {
+  console.log("The eather is bad!");
+}
