c6c9177357373e28363a14251a10a7570f856c2b
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-06-22 15:52:45 -0300

    commit

--- BroCode/39)javascriptCallbacks/index.js
+++ BroCode/39)javascriptCallbacks/index.js
@@ -1,5 +1,10 @@
 // callback = a function passed as an argument to another function
 
+// Ensures that a function is not going to run before a task is completed
+// helps us develop asynchronous code(when one function has to wait for another function).
+// that helps us avoid errors and potential problems
+// EX: wait for file to load
+
 sum(2, 3, displayDOM); // third is a callback
 
 function sum(x, y, callback) {
