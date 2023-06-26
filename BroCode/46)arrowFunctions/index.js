acebd5384ef40ae1356219be980969105cc20aaf
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-06-26 19:11:08 -0300

    commit

---
+++ BroCode/46)arrowFunctions/index.js
@@ -0,0 +1,23 @@
+// compact alternative toa tradicional function expression
+
+// const greeting = function (username){
+//     console.log(`Hello ${username}`)
+// }
+
+// greeting("Bro");
+
+//==============================================================
+//ARROW 1
+
+// const greeting = (username) => console.log(`Hello ${username}`);
+
+// greeting(bro);
+
+//==============================================================
+//ARROW 2
+
+// const percent = (x, y) => x/y * 100;
+
+// console.log(`${percent(45,50)}%`)
+
+//==============================================================
