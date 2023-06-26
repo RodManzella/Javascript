d08c3ece3b7d587aa311d9c9a7db13388576d2e9
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-06-26 14:11:29 -0300

    commit

---
+++ BroCode/45)functionExpression/index.js
@@ -0,0 +1,3 @@
+// function expression = function without a name (anonymous function)
+//                       avoid polluting the global scope with names
+//                       write it, then forget about it.
