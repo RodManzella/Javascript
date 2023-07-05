4f6c460b39282eed861537b4aa7a22c2f479c8e5
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-07-05 09:54:34 -0300

    commit

---
+++ BroCode/50)javascriptMaps/index.js
@@ -0,0 +1,18 @@
+// Map = object that holds key-value pairs of any data type.
+
+const store = new Map([
+  ["t-shirt", 20],
+  ["jeans", 30],
+  ["socks", 10],
+  ["underwear", 50],
+]);
+
+let shoppingCart = 0;
+
+// store.get("t-shirt");
+// store.set("hat", 40);
+// store.delete("hat");
+// console.log(store.has("underwear"));
+console.log(store.size);
+
+store.forEach((value, key) => console.log(`${key} ${value}`));
